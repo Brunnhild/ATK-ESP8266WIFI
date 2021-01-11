88016a372dffa350df7791d7e676ae3d63d2e118
Brunnhild <10175101259@stu.ecnu.edu.cn>
2021-01-11 14:10:09 +0800

    Format timer.c

--- HARDWARE/TIMER/timer.c
+++ HARDWARE/TIMER/timer.c
@@ -6,106 +6,87 @@
 #include "adc3.h"
 #include "lsens.h"
 #include "app.h"
-//////////////////////////////////////////////////////////////////////////////////	 
-//������ֻ��ѧϰʹ�ã�δ���������ɣ��������������κ���;
-//ALIENTEK STM32F407������
-//��ʱ�� ��������	   
-//����ԭ��@ALIENTEK
-//������̳:www.openedv.com
-//��������:2014/5/4
-//�汾��V1.0
-//��Ȩ���У�����ؾ���?
-//Copyright(C) �������������ӿƼ����޹�˾ 2014-2024
-//All rights reserved									  
-////////////////////////////////////////////////////////////////////////////////// 	 
 
 short temp; 
 u8 adcx;
-extern vu16 USART3_RX_STA;
+extern u16 USART3_RX_STA;
 
-//��ʱ��7�жϷ������?		    
 void TIM7_IRQHandler(void)
-{ 	
-	if (TIM_GetITStatus(TIM7, TIM_IT_Update) != RESET)//�Ǹ����ж�
-	{	 			   
-		USART3_RX_STA|=1<<15;	//��ǽ������
-		TIM_ClearITPendingBit(TIM7, TIM_IT_Update  );  //���TIM7�����жϱ�־    
-		TIM_Cmd(TIM7, DISABLE);  //�ر�TIM7 
-	}	    
-}
- 
-//ͨ�ö�ʱ���жϳ�ʼ��
-//����ʼ��ѡ��ΪAPB1��2������APB1Ϊ36M
-//arr���Զ���װֵ��
-//psc��ʱ��Ԥ��Ƶ��		 
-void TIM7_Int_Init(u16 arr,u16 psc)
-{	
-	NVIC_InitTypeDef NVIC_InitStructure;
-	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
-
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM7, ENABLE);//TIM7ʱ��ʹ��    
-	
-	//��ʱ��TIM7��ʼ��
-	TIM_TimeBaseStructure.TIM_Period = arr; //��������һ�������¼�װ���?���Զ���װ�ؼĴ������ڵ�ֵ	
-	TIM_TimeBaseStructure.TIM_Prescaler =psc; //����������ΪTIMxʱ��Ƶ�ʳ�����Ԥ��Ƶֵ
-	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; //����ʱ�ӷָ�:TDTS = Tck_tim
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM���ϼ���ģʽ
-	TIM_TimeBaseInit(TIM7, &TIM_TimeBaseStructure); //����ָ���Ĳ�����ʼ��TIMx��ʱ��������?
- 
-	TIM_ITConfig(TIM7,TIM_IT_Update,ENABLE ); //ʹ��ָ����TIM7�ж�,���������ж�
-
-	 	  
-	NVIC_InitStructure.NVIC_IRQChannel = TIM7_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0 ;//��ռ���ȼ�0
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;		//�����ȼ�1
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQͨ��ʹ��
-	NVIC_Init(&NVIC_InitStructure);	//����ָ���Ĳ�����ʼ��VIC�Ĵ���
-	
-}
-	
-void TIM3_Int_Init(u16 arr,u16 psc)
 {
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
-	NVIC_InitTypeDef NVIC_InitStructure;
-	
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3,ENABLE);  ///ʹ��TIM3ʱ��
-	
-  TIM_TimeBaseInitStructure.TIM_Period = arr; 	//�Զ���װ��ֵ
-	TIM_TimeBaseInitStructure.TIM_Prescaler=psc;  //��ʱ����Ƶ
-	TIM_TimeBaseInitStructure.TIM_CounterMode=TIM_CounterMode_Up; //���ϼ���ģʽ
-	TIM_TimeBaseInitStructure.TIM_ClockDivision=TIM_CKD_DIV1; 
-	
-	TIM_TimeBaseInit(TIM3,&TIM_TimeBaseInitStructure);//��ʼ��TIM3
-	
-	TIM_ITConfig(TIM3,TIM_IT_Update,ENABLE); //������ʱ��3�����ж�
-	TIM_Cmd(TIM3,ENABLE); //ʹ�ܶ�ʱ��3
-	
-	NVIC_InitStructure.NVIC_IRQChannel=TIM3_IRQn; //��ʱ��3�ж�
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0x01; //��ռ���ȼ�1
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority=0x03; //�����ȼ�3
-	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
+    if (TIM_GetITStatus(TIM7, TIM_IT_Update) != RESET)
+    {
+        USART3_RX_STA |= 1 << 15;
+        TIM_ClearITPendingBit(TIM7, TIM_IT_Update);
+        TIM_Cmd(TIM7, DISABLE);
+    }
+}
+
+void TIM7_Int_Init(u16 arr, u16 psc)
+{
+    NVIC_InitTypeDef NVIC_InitStructure;
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM7, ENABLE);
+
+    TIM_TimeBaseStructure.TIM_Period = arr;
+    TIM_TimeBaseStructure.TIM_Prescaler = psc;
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM7, &TIM_TimeBaseStructure);
+
+    TIM_ITConfig(TIM7, TIM_IT_Update, ENABLE);
+
+    NVIC_InitStructure.NVIC_IRQChannel = TIM7_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+void TIM3_Int_Init(u16 arr, u16 psc)
+{
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+    TIM_TimeBaseInitStructure.TIM_Period = arr;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = psc;
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);
+
+    TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM3, ENABLE);
+
+    NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x03;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
 }
 
 // ADC handler
 void TIM3_IRQHandler(void)
 {
-	if(TIM_GetITStatus(TIM3,TIM_IT_Update)==SET) //����ж�?
-	{
-		temp=Get_Temprate();	//�õ��¶�ֵ 
-		if(temp<0)
-		{
-			temp=-temp;
-			LCD_ShowString(30+10*8,110,16,16,16,"-");	    //��ʾ����
-		}else LCD_ShowString(30+10*8,110,16,16,16," ");	//�޷���
-		
-		LCD_ShowxNum(30+11*8,110,temp/100,2,16,0);		//��ʾ��������
-		LCD_ShowxNum(30+14*8,110,temp%100,2,16,0);		//��ʾС������ 
-		
-		
-		adcx=Lsens_Get_Val();
-		LCD_ShowxNum(30+10*8,90,adcx,3,16,0);//��ʾADC��ֵ 
-		LED1=!LED1;
-	}
-	TIM_ClearITPendingBit(TIM3,TIM_IT_Update);  //����жϱ�־�?
+    if (TIM_GetITStatus(TIM3, TIM_IT_Update) == SET)
+    {
+        temp = Get_Temprate();
+        if (temp < 0)
+        {
+            temp = -temp;
+            LCD_ShowString(30 + 10 * 8, 110, 16, 16, 16, "-");
+        }
+        else
+            LCD_ShowString(30 + 10 * 8, 110, 16, 16, 16, " ");
+
+        LCD_ShowxNum(30 + 11 * 8, 110, temp / 100, 2, 16, 0);
+        LCD_ShowxNum(30 + 14 * 8, 110, temp % 100, 2, 16, 0);
+
+        adcx = Lsens_Get_Val();
+        LCD_ShowxNum(30 + 10 * 8, 90, adcx, 3, 16, 0);
+        LED1 = !LED1;
+    }
+    TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 }
